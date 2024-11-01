f104bfba596fb4fac7516d13d115bbd1eb664908
tuanhao2003 <nhtuanhao@gmail.com>
2024-11-01 22:27:58 +0700

    update 1/11

--- src/main/java/com/example/ticketbooker/DTO/Trips/AddTripDTO.java
+++ src/main/java/com/example/ticketbooker/DTO/Trips/AddTripDTO.java
@@ -1,16 +1,21 @@
 package com.example.ticketbooker.DTO.Trips;
 
+import com.example.ticketbooker.Entity.Buses;
+import com.example.ticketbooker.Entity.Driver;
+import com.example.ticketbooker.Entity.Routes;
 import com.example.ticketbooker.Util.Enum.TripStatus;
 import jakarta.persistence.Column;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class AddTripDTO {
-    private Integer routeId;
-    private Integer busId;
-    private Integer driverId;
+    private Routes route;
+    private Buses bus;
+    private Driver driver;
     private String departureStation;
     private String arrivalStation;
     private LocalDateTime departureTime;
@@ -20,15 +25,27 @@ public class AddTripDTO {
     private TripStatus tripStatus; // Có thể thêm trạng thái nếu cần
 
     public AddTripDTO() {
-        this.routeId = null;
-        this.busId = null;
-        this.driverId = null;
+        this.route = null;
+        this.bus = null;
+        this.driver = null;
         this.departureStation = "";
         this.arrivalStation = "";
         this.departureTime = LocalDateTime.now();
         this.arrivalTime = null;
         this.price = null;
         this.availableSeats = null;
-        this.tripStatus = TripStatus.Scheduled; // Trạng thái mặc định
+        this.tripStatus = TripStatus.SCHEDULED; // Trạng thái mặc định
+    }
+    public AddTripDTO(Routes routeId, Buses busId, Driver driverId, String departureStation, String arrivalStation, LocalDateTime departureTime, LocalDateTime arrivalTime, Integer price, Integer availableSeats, TripStatus tripStatus) {
+        this.route = routeId;
+        this.bus = busId;
+        this.driver = driverId;
+        this.departureStation = departureStation;
+        this.arrivalStation = arrivalStation;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.price = price;
+        this.availableSeats = availableSeats;
+        this.tripStatus = tripStatus;
     }
 }
--- src/main/java/com/example/ticketbooker/Util/Mapper/TripMapper.java
+++ src/main/java/com/example/ticketbooker/Util/Mapper/TripMapper.java
@@ -1,36 +1,46 @@
 package com.example.ticketbooker.Util.Mapper;
 
+
 import com.example.ticketbooker.DTO.Trips.AddTripDTO;
+import com.example.ticketbooker.DTO.Trips.RequestIdTripDTO;
 import com.example.ticketbooker.DTO.Trips.UpdateTripDTO;
 import com.example.ticketbooker.Entity.Trips;
+import com.example.ticketbooker.Repository.TripRepo;
+import com.example.ticketbooker.Service.TripService;
 import com.example.ticketbooker.Util.Enum.TripStatus;
-public class TripMapper {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.ArrayList;
+
+public class TripMapper {
     public static Trips fromAdd(AddTripDTO dto) {
+
         return Trips.builder()
-                .routeId(dto.getRouteId())
-                .busId(dto.getBusId())
-                .driverId(dto.getDriverId())
+                .route(dto.getRoute())
+                .bus(dto.getBus())
+                .driver(dto.getDriver())
                 .departureStation(dto.getDepartureStation())
                 .arrivalStation(dto.getArrivalStation())
-                .departureTime(dto.getDepartureTime())
-                .arrivalTime(dto.getArrivalTime())
+                .departureTime(Instant.from(dto.getDepartureTime()))
+                .arrivalTime(Instant.from(dto.getArrivalTime()))
                 .price(dto.getPrice())
                 .availableSeats(dto.getAvailableSeats())
-                .tripStatus(dto.getTripStatus() != null ? dto.getTripStatus() : TripStatus.Scheduled) // Sử dụng trạng thái mặc định
+                .tripStatus(dto.getTripStatus() != null ? dto.getTripStatus() : TripStatus.SCHEDULED) // Sử dụng trạng thái mặc định
                 .build();
     }
 
-    public static Trips fromUpdate(int id, UpdateTripDTO dto) {
+    public static Trips fromUpdate(UpdateTripDTO dto) {
         return Trips.builder()
-                .tripId(id)
-                .routeId(dto.getRouteId())
-                .busId(dto.getBusId())
-                .driverId(dto.getDriverId())
+                .id(dto.getTripId())
+                .route(dto.getRoute())
+                .bus(dto.getBus())
+                .driver(dto.getDriver())
                 .departureStation(dto.getDepartureStation())
                 .arrivalStation(dto.getArrivalStation())
-                .departureTime(dto.getDepartureTime())
-                .arrivalTime(dto.getArrivalTime())
+                .departureTime(Instant.from(dto.getDepartureTime()))
+                .arrivalTime(Instant.from(dto.getArrivalTime()))
                 .price(dto.getPrice())
                 .availableSeats(dto.getAvailableSeats())
                 .tripStatus(dto.getTripStatus()) // Cập nhật trạng thái nếu có
