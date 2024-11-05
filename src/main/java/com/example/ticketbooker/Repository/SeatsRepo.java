e664ece28c8503031a020209215a4e24dedf27b4
tuanhao2003 <nhtuanhao@gmail.com>
2024-11-05 22:42:36 +0700

    fix bug tai loc

--- src/main/java/com/example/ticketbooker/Repository/SeatsRepo.java
+++ src/main/java/com/example/ticketbooker/Repository/SeatsRepo.java
@@ -1,7 +1,6 @@
 package com.example.ticketbooker.Repository;
 
 import com.example.ticketbooker.Entity.Seats;
-import com.example.ticketbooker.Util.Enum.SeatStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +8,4 @@ import java.util.ArrayList;
 
 @Repository
 public interface SeatsRepo extends JpaRepository<Seats, Integer> {
-    ArrayList<Seats> findByTripIdAndSeatStatus(Integer tripId, SeatStatus seatStatus);  // Lấy chỗ còn trống theo chuyến
 }
