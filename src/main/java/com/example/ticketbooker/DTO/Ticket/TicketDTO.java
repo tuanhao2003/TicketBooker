b7eb1cca0df2950cbdd7a7eb5df6f6bac181db21
Nguyễn Chí Tài <133089031+dannychtai@users.noreply.github.com>
2024-11-07 10:49:10 +0700

    Manage Ticket

--- src/main/java/com/example/ticketbooker/DTO/Ticket/TicketDTO.java
+++ src/main/java/com/example/ticketbooker/DTO/Ticket/TicketDTO.java
@@ -1,41 +1,28 @@
 package com.example.ticketbooker.DTO.Ticket;
 
+import com.example.ticketbooker.Entity.Account;
+import com.example.ticketbooker.Entity.Invoices;
+import com.example.ticketbooker.Entity.Seats;
+import com.example.ticketbooker.Entity.Trips;
 import com.example.ticketbooker.Util.Enum.TicketStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TicketDTO {
     private Integer id;
-    private Integer tripId;
-    private Integer bookerId;
-    private Integer invoiceId;
+    private Trips tripId;
+    private Account bookerId;
+    private Invoices invoiceId;
     private String customerName;
     private String customerPhone;
-    private Integer seatId;
+    private Seats seatId;
     private String qrCode;
     private TicketStatus ticketStatus;
-
-    public TicketDTO() {
-        this.id = null;
-        this.tripId = null;
-        this.bookerId = null;
-        this.invoiceId = null;
-        this.customerName = null;
-        this.customerPhone = null;
-        this.seatId = null;
-        this.qrCode = null;
-        this.ticketStatus = null;
-    }
-
-    public TicketDTO(Integer id, Integer tripId, Integer bookerId, Integer invoiceId, String customerName, String customerPhone, Integer seatId, String qrCode, TicketStatus ticketStatus) {
-        this.id = id;
-        this.tripId = tripId;
-        this.bookerId = bookerId;
-        this.invoiceId = invoiceId;
-        this.customerName = customerName;
-        this.customerPhone = customerPhone;
-        this.seatId = seatId;
-        this.qrCode = qrCode;
-        this.ticketStatus = ticketStatus;
-    }
 }
+
