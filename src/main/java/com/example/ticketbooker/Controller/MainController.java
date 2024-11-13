e096c2a3e59b3c2e85b4929d0fac0b8183e78a39
Thanhloc913 <huynhthanhloc913@gmail.com>
2024-11-13 17:07:59 +0700

    Giao dien trang chu + cac trang con

---
+++ src/main/java/com/example/ticketbooker/Controller/MainController.java
@@ -0,0 +1,28 @@
+package com.example.ticketbooker.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/fuba")
+public class MainController {
+    @GetMapping()
+    public String showMainPage() {
+        return "View/User/TrangChu";
+    }
+
+    @GetMapping("/about-us")
+    public String showAboutUs() {
+        return "View/User/AboutUs";
+    }
+
+    @GetMapping("/contact-us")
+    public String showContactUs() {
+        return "View/User/ContactUs";
+    }
+
+    @GetMapping("/ticket-lookup")
+    public String showTicketLookup() {
+        return "View/User/LookUpTicket";
+    }
+}
