488d362bb1cc62f5bdd64d977b4f2b9a41a75f4c
NKaii404 <nnhatkhai83@gmail.com>
2024-11-14 10:04:49 +0700

    login-html

--- src/main/java/com/example/ticketbooker/Repository/DriverRepo.java
+++ src/main/java/com/example/ticketbooker/Repository/DriverRepo.java
@@ -5,7 +5,6 @@ import com.example.ticketbooker.Entity.Driver;
 import com.example.ticketbooker.Util.Enum.DriverStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -18,14 +17,13 @@ public interface DriverRepo  extends JpaRepository<Driver, Integer> {
     ArrayList<Driver> findAllDriversByName(String name);
     ArrayList<Driver> findAllDriversByDriverStatus(DriverStatus status);
     ArrayList<Driver> findAllDriversByPhone(String phone);
-    @Query(
-            value = "SELECT * FROM Driver d WHERE LOWER(d.name) LIKE LOWER(CONCAT('%', ?1 , '%')) OR " +
-            "LOWER(d.licenseNumber) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
-            "LOWER(d.phone) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
-            "LOWER(d.address) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
-            "CAST(d.driverId AS CHAR) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
-            "LOWER(d.driverStatus) LIKE LOWER(CONCAT('%', ?1, '%'))",
-            nativeQuery = true)
+    @Query("SELECT d FROM Driver d WHERE " +
+            "LOWER(d.name) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(d.licenseNumber) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(d.phone) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(d.address) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "CAST(d.driverId AS string) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(d.driverStatus) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
     ArrayList<Driver> searchDrivers(String searchTerm);
 
 }
