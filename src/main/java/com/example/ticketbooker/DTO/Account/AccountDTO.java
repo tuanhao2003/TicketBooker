d975aa4ca7b535d0dadb3440fa16d9f48b3d15a1
Nguyễn Chí Tài <133089031+dannychtai@users.noreply.github.com>
2024-11-22 00:17:18 +0700

    Account function

--- src/main/java/com/example/ticketbooker/DTO/Account/AccountDTO.java
+++ src/main/java/com/example/ticketbooker/DTO/Account/AccountDTO.java
@@ -3,12 +3,14 @@ package com.example.ticketbooker.DTO.Account;
 import com.example.ticketbooker.Util.Enum.AccountStatus;
 import com.example.ticketbooker.Util.Enum.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AccountDTO {
     private Integer id;
     private String username;
---
+++ src/main/java/com/example/ticketbooker/Service/ServiceImp/AccountServiceImp.java
@@ -0,0 +1,63 @@
+package com.example.ticketbooker.Service.ServiceImp;
+
+import com.example.ticketbooker.DTO.Account.AccountDTO;
+import com.example.ticketbooker.Entity.Account;
+import com.example.ticketbooker.Repository.AccountRepo;
+import com.example.ticketbooker.Service.AccountService;
+import com.example.ticketbooker.Util.Mapper.AccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AccountServiceImp implements AccountService {
+
+    @Autowired
+    private AccountRepo accountRepo;
+
+    @Override
+    public List<AccountDTO> getAllAccounts() {
+        List<Account> accounts = accountRepo.findAll();
+        List<AccountDTO> dtos = new ArrayList<>();
+        accounts.forEach(account -> dtos.add(AccountMapper.toDTO(account)));
+        return dtos;
+    }
+
+    @Override
+    public AccountDTO getAccountById(int id) {
+        AccountDTO result = new AccountDTO();
+        try {
+            result = AccountMapper.toDTO(this.accountRepo.findById(id));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return result;
+        }
+        return result;
+    }
+
+    @Override
+    public AccountDTO createAccount(AccountDTO accountDTO) {
+        Account account = AccountMapper.toEntity(accountDTO);
+        Account savedAccount = accountRepo.save(account);
+        return AccountMapper.toDTO(accountRepo.save(account));
+    }
+
+    @Override
+    public boolean updateAccount(AccountDTO accountDTO) {
+        try {
+            Account account = AccountMapper.toEntity(accountDTO);
+            this.accountRepo.save(account);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void deleteAccount(Integer id) {
+        accountRepo.deleteById(id);
+    }
+}
