923bb056f26299a610a81525c283d11e6e0d3113
nKay83 <khaisx99@gmail.com>
2024-11-27 00:22:27 +0700

    Đăng nhập spring sercurity

---
+++ src/main/java/com/example/ticketbooker/Entity/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.example.ticketbooker.Entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Mặc định mình sẽ để tất cả là ROLE_USER. Để demo cho đơn giản.
+        return Collections.singleton(new SimpleGrantedAuthority(this.account.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
