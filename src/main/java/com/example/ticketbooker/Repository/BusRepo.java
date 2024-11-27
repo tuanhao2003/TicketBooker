69c664ae62813135058cdc66e38c8d64f1476466
Nguyễn Chí Tài <133089031+dannychtai@users.noreply.github.com>
2024-11-28 02:47:39 +0700

    BUS SEARCH

--- src/main/java/com/example/ticketbooker/Repository/BusRepo.java
+++ src/main/java/com/example/ticketbooker/Repository/BusRepo.java
@@ -1,6 +1,8 @@
 package com.example.ticketbooker.Repository;
 
 import com.example.ticketbooker.Entity.Buses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,6 @@ public interface BusRepo extends JpaRepository<Buses, Integer> {
     Buses findById(int id);
     List<Buses> findAll();
     Optional<Buses> findByLicensePlate(String licensePlate); // New method
+    Page<Buses> findByLicensePlateContainingIgnoreCase(String licensePlate, Pageable pageable);
 }
 
