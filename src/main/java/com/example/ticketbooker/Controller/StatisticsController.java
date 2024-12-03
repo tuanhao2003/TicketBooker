79963907211ed39283a5ef1395039c50a72c9031
Nguyễn Chí Tài <133089031+dannychtai@users.noreply.github.com>
2024-12-03 18:34:48 +0700

    update Statistics

--- src/main/java/com/example/ticketbooker/Controller/StatisticsController.java
+++ src/main/java/com/example/ticketbooker/Controller/StatisticsController.java
@@ -20,13 +20,15 @@ public class StatisticsController {
     private InvoiceService invoiceService;
     @GetMapping()
     public String revenueStats(
-            @RequestParam("period") String period,
-            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate selectedDate,
+            @RequestParam(value = "period", defaultValue = "Day") String period,  // Default period to 'Day'
+            @RequestParam(value = "date", defaultValue = "#{T(java.time.LocalDate).now()}")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate selectedDate,  // Default date to today's date
             Model model) {
 
-
+        // Lấy thông tin doanh thu dựa trên period và date đã được xác định
         RevenueStatsDTO stats = invoiceService.getRevenueStats(period, selectedDate);
         model.addAttribute("stats", stats);
-        return "View/Admin/Statistics/Stats"; // Create this Thymeleaf template
+
+        return "View/Admin/Statistics/Stats";  // Trả về view của Thymeleaf
     }
 }
